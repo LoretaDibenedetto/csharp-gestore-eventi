15745d4ccbdaebb33fb4a233b033ff2ef665959d
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-28 10:18:55 +0200

    add get e set to class evento and evento constructor

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -9,21 +9,60 @@ namespace GestoreEventi
     public class Evento
     {
         private string titolo;
-        private DateTime dateTime;
-        private int massimaCapienza;
+        private DateTime data;
+        private int massimaCapienza = 200;
         private int postiPrenotati;
 
         public string GetTitolo
         {
             get { return titolo; }
-            set { titolo = value; }
+            set
+            {
+                if (titolo == "")
+                {
+                    throw new ArgumentException("Il titolo non può essere vuoto.");
+                }
+                titolo = value;
+            }
         }
 
         public DateTime GetDateTime
         {
-            get { return dateTime; }
-            set { dateTime = value; }
+            get { return data; }
+            set
+            {
+                if (value < DateTime.Today)
+                {
+                    throw new ArgumentException("La data dell'evento non può essere nel passato.");
+                }
+                data = value;
+            }
         }
+
+        public int GetMassimaCapienza
+        {
+            get { return massimaCapienza; }
+            
+        }
+
+        public int GetPostiPrenotati
+        {
+            get { return postiPrenotati; }
+        }
+
+
+        public Evento(string titolo, DateTime data, int massimacapienza, int postiPrenotati = 0) 
+        {
+            this.titolo = titolo;
+            this.data = data;
+            this.massimaCapienza= massimacapienza;
+            this.postiPrenotati= postiPrenotati;
+        
+        
+        
+        }
+
+
 
 
     }
