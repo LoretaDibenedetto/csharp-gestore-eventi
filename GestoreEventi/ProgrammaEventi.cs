ec5f84d96323a11e1e5975bd55d0fc42e928ff82
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-28 11:47:44 +0200

    add new class ProgrammaEventi and add list of eventi and title attribute, constructor and getting method

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class ProgrammaEventi
+    {
+        string Titolo;
+        List<Evento> eventi;
+
+
+        public ProgrammaEventi( string Titolo)
+        {
+            this.Titolo = Titolo;
+            eventi = new List<Evento>();
+        }
+
+        public string GetTitolo
+        {
+            get { return Titolo; }
+        }
+
+        public List<Evento> GetEventi()
+        {
+            return eventi;
+        }
+
+
+
+
+    }
+}
