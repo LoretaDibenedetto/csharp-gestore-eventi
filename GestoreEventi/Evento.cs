6327d7827b01e6493ec8666dfb9a1d50c49ed728
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-28 09:57:47 +0200

    Add project files.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Evento
+    {
+        private string titolo;
+        private DateTime dateTime;
+        private int massimaCapienza;
+        private int postiPrenotati;
+
+        public string GetTitolo
+        {
+            get { return titolo; }
+            set { titolo = value; }
+        }
+
+        public DateTime GetDateTime
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+
+
+    }
+}
